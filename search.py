c8159ccca9ee0801aaf53e966423eef55e8eae15
Ben Payne <ben.is.located@gmail.com>
2020-05-20 07:30:29 -0400

    revised input arguments, added lots of documentation

--- search.py
+++ search.py
@@ -1,11 +1,42 @@
-def regex_and_or(pattern,delimiter='\s+',data=''):
-    import re
+#!/usr/bin/env python3
+
+import re
+
+def regex_and_or(pattern, list_of_strings_to_search, delimiter='\s+'):
+    """
+    # when multiple terms are present in the pattern, 
+    # then search "term1 AND term2 AND term3" in the list of strings
+
+    # default is to split each string in the corpus on spaces
+
+    # "data" is a list of strings
+
+    >>> list_of_strings_to_search = ["Normally matches any character except a newline.", "Within square brackets the dot is literal."]
+    >>> regex_and_or('(Normally|With) (anyf|char)? square', list_of_strings_to_search)
+    ['Within square brackets the dot is literal. ']
+
+    # match one of the two
+    >>> regex_and_or('Normally|With', list_of_strings_to_search)
+    ['Normally matches any character except a newline.', 'Within square brackets the dot is literal. ']
+
+    # The question mark makes the preceding token in the regular expression optional. 
+    # https://www.regular-expressions.info/optional.html
+    >>> regex_and_or('(anyf|char)?', list_of_strings_to_search)
+    ['Normally matches any character except a newline.', 'Within square brackets the dot is literal. ']
+ 
+    # exact match
+    >>> regex_and_or('square', list_of_strings_to_search)
+    ['Within square brackets the dot is literal. ']
+
+    # look for a math expression that has both "cos" and "2" present
+    >>> list_of_strings_to_search = ['sin(x) + cos(2x) = f(x)', 'ax^2 + bx + c = 0']
+    >>> regex_and_or('cos 2', list_of_strings_to_search)
+    ['sin(x) + cos(2x) = f(x)']
+    """
     ands = re.split(delimiter,pattern)
-    data = """Normally matches any character except a newline.
-Within square brackets the dot is literal. """
+
     match_list = []
-    
-    for line in data.splitlines():
+    for line in list_of_strings_to_search:
         ignore_line = False
         for word in ands:
             if not re.findall(word,line):
@@ -14,6 +45,4 @@ Within square brackets the dot is literal. """
             match_list.append(line)
     return match_list
 
-
-regex_and_or('(Normally|With) (anyf|char)? square')
-#['Within square brackets the dot is literal. ']
+# EOF
