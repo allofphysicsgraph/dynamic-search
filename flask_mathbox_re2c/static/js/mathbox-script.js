9c34abb5aca4c8bb10d1bf5681db8f561ba5554b
mike <mike@mathrules.us>
2025-02-08 18:33:22 +0000

    neo4j query using neo4j-driver npm

--- flask_mathbox_re2c/static/js/mathbox-script.js
+++ flask_mathbox_re2c/static/js/mathbox-script.js
@@ -1,5 +1,5 @@
-var button = document.querySelector("#button");
-var symbols = [
+let button = document.querySelector("#button");
+let symbols = [
     "\\alpha",
     "\\theta",
     "\\tau",
@@ -7,105 +7,84 @@ var symbols = [
     "\\eta",
     "i",
 ];
-
-// Define VDOM handler to clone real DOM elements
-var clone = MathBox.DOM.createClass({
-    render: function (el, props, children) {
-        var button = children.cloneNode(true);
-        return button;
-    },
-});
-
-// Define VDOM handler to format 'latex' into an HTML span
-var latex = MathBox.DOM.createClass({
+// Define global DOM handler to format 'latex' into an HTML span
+MathBox.DOM.Types.latex = MathBox.DOM.createClass({
     render: function (el) {
         this.props.innerHTML = katex.renderToString(this.children);
         return el("span", this.props);
     },
 });
 
-var mathbox = MathBox.mathBox({
-    plugins: ["core", "controls", "cursor"],
+mathbox = MathBox.mathBox({
+    plugins: ["core", "controls", "cursor", "stats"],
     controls: {
         klass: THREE.OrbitControls,
-    },
-    loop: {
-        start: window == window.top,
     },
     camera: {
         near: 0.01,
         far: 1000,
     },
 });
-var three = mathbox.three;
+three = mathbox.three;
 
-three.camera.position.set(-1, 1, 2.5);
+three.camera.position.set(1.1, 1.45, 1);
 three.camera.lookAt(new THREE.Vector3());
 three.renderer.setClearColor(new THREE.Color(0xffffff), 1.0);
 
-var view = mathbox
-    .set({
+view = mathbox
+    .unit({
         scale: null,
     })
-    .polar({
+    .cartesian({
         range: [
             [-2, 2],
             [-1, 1],
             [-1, 1],
         ],
         scale: [2, 1, 1],
-        bend: 0.25,
     });
 
-var node_count = 32;
+
+
+
+view.interval({
+    width: 1,
+    expr: function (emit, x, i, time) {
+        y = Math.sin(x + (time * 0.2) / 4) * 0.7;
+        emit(x, y);
+    },
+    channels: 2,
+});
+
+
 view
-    .interval({
-        width: 2*node_count,
-        expr: function (emit, x, i, t) {
-            y = Math.sin(x + t / 4) * 0.5 + 0.75;
-            emit(x, y);
-        },
-        channels: 2,
-    })
-    .resample({
-        width: node_count,
-    })
     .point({
-        color: 0x30c0ff,
-        size: 80,
+        color: 0x3090ff,
+        size: 40,
     })
+
     .html({
-        width: node_count,
-        height: 2,
-        expr: function (emit, el, i, j, k, l, t) {
-            // Emit latex
-            var color = ["#30D0FF", "#30A0FF"][i % 2];
-            var a =
-                Math.round(
-                    Math.sqrt(t * t + t + 3) +
-                    Math.abs(Math.cos(t * 0.1 + i + i * i) * 50)
-                ) % symbols.length;
-            var b = Math.round(Math.sqrt(t * t + Math.sin(t + i * i) + 5));
+        width: 1,
+        height: 3,
+        depth: 2,
+        expr: function (emit, el, i, j, k, l, time) {
+            // Emit latex element
             emit(
                 el(
-                    latex,
-                    { style: { color: color } },
-                    "\\sqrt{L^AT_EX + " +
-                    (i + b + 1) +
-                    " \\pi^{" +
-                    symbols[a] +
-                    "}}"
+                    "latex",
+                    null,
+                    "c = \\mathbf{F} \\pm\\sqrt{" + i + " a^2 + " + j + " b^2}"
                 )
             );
         },
     })
     .dom({
         snap: false,
-        offset: [0, 32],
-        depth: 0,
+        offset: [0, -32],
+        depth: 0.5,
+        size: 24,
         zoom: 1,
         outline: 2,
-        size: 20,
     });
 
 
